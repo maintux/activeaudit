e41204546af018b6a893e8cb1bd6d51dceb3e589
Massimo Maino <maintux@gmail.com>
2013-02-22 17:06:55 +0100

    add support for audit model concern

--- app/models/active_audit/audit.rb
+++ app/models/active_audit/audit.rb
@@ -3,4 +3,9 @@ class ActiveAudit::Audit < ActiveRecord::Base
   attr_accessible :obj_id, :obj_type, :activity, :user_id
   validates_presence_of :obj_id, :obj_type, :activity
 
+  ::ActiveAudit::AuditConcern
+  if defined?(::ActiveAudit::AuditConcern)
+    include ::ActiveAudit::AuditConcern
+  end
+
 end
--- lib/active_audit/engine.rb
+++ lib/active_audit/engine.rb
@@ -3,6 +3,9 @@ require 'active_audit/logger'
 module ActiveAudit
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'activeservice.autoload', :before => :set_autoload_paths do |app|
+        app.config.autoload_paths += %W(#{app.config.root}/app/concerns/active_audit)
+      end
       #ActiveRecord::Base.class_eval do
         #include ActiveAudit::Logger
       #end
