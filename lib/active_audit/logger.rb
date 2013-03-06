fc02a755cbc506f80bdbd74ff108d6bf49c52484
Massimo Maino <maintux@gmail.com>
2013-03-06 21:33:53 +0100

    refactoring logger

--- lib/active_audit/logger.rb
+++ lib/active_audit/logger.rb
@@ -25,23 +25,23 @@ module ActiveAudit
         self.loggable_events.merge! event
       end
 
+      def log_event(activity)
+        ActiveAudit::Audit.create obj_id: id, obj_type: self.class.to_s, user_id: audit_user_id, activity: activity, extras: audit_extras
+      end
+      private :log_event
+
       def log_activity_on_create
         self.class.loggable_events.each do |k,v|
           next unless k.eql?(:create)
           if v.eql?(true)
-            ActiveAudit::Audit.create obj_id: id, obj_type: self.class.to_s, user_id: audit_user_id, activity: k.to_s, extras: audit_extras
+            log_event k.to_s
           elsif v.is_a?(Array)
-            log_event = false
             v.each do |field|
-              if field.is_a?(Hash)
-                key, value = field.first
-                log_event = true and break if self.send(key.to_s).eql?(value)
-              elsif field.is_a?(Symbol)
-                log_event and break if self.send(field.to_s)
+              if field.is_a?(Hash) and self.send(field.keys.first).eql?(field.values.first)
+                log_event(k.to_s) and break
+              elsif field.is_a?(Symbol) and self.send(field.to_s)
+                log_event(k.to_s) and break
               end
-            end
-            if log_event
-              ActiveAudit::Audit.create obj_id: id, obj_type: self.class.to_s, user_id: audit_user_id, activity: k.to_s, extras: audit_extras
             end
           end
         end
@@ -51,20 +51,13 @@ module ActiveAudit
         self.class.loggable_events.each do |k,v|
           next if [:create,:destroy].include?(k)
           if k.eql?(:update) and v.eql?(true)
-            ActiveAudit::Audit.create obj_id: id, obj_type: self.class.to_s, user_id: audit_user_id, activity: k.to_s, extras: audit_extras
-          else
-            if v.is_a?(Array)
-              log_event = false
-              v.each do |field|
-                if field.is_a?(Hash)
-                  key, value = field.first
-                  log_event = true and break if self.send("#{key}_changed?") and self.send(key.to_s).eql?(value)
-                elsif field.is_a?(Symbol)
-                  log_event = true and break if self.send("#{field}_changed?")
-                end
-              end
-              if log_event
-                ActiveAudit::Audit.create obj_id: id, obj_type: self.class.to_s, user_id: audit_user_id, activity: k.to_s, extras: audit_extras
+            log_event k.to_s
+          elsif v.is_a?(Array)
+            v.each do |field|
+              if field.is_a?(Hash) and self.send("#{field.keys.first}_changed?") and self.send(field.keys.first).eql?(field.values.first)
+                log_event(k.to_s) and break
+              elsif field.is_a?(Symbol) and self.send("#{field}_changed?")
+                log_event(k.to_s) and break
               end
             end
           end
@@ -74,7 +67,7 @@ module ActiveAudit
       def log_activity_on_destroy
         self.class.loggable_events.each do |k,v|
           next unless k.eql?(:destroy)
-          ActiveAudit::Audit.create obj_id: id, obj_type: self.class.to_s, user_id: audit_user_id, activity: k.to_s, extras: audit_extras
+          log_event k.to_s
         end
       end
 
