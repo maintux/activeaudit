19a781abdf9219a005926f12108a37abe5461c46
Massimo Maino <maintux@gmail.com>
2013-03-06 15:25:35 +0100

    fix class variable to prevent overlapping between models

--- lib/active_audit/logger.rb
+++ lib/active_audit/logger.rb
@@ -17,18 +17,16 @@ module ActiveAudit
       attr_accessor :audit_user_id
       attr_accessor :audit_extras
 
-      @@_loggable_events = {}
-
-      def self.loggable_event(event)
-        @@_loggable_events.merge! event
+      cattr_accessor :loggable_events do
+        {}
       end
 
-      def self.loggable_events
-        @@_loggable_events
+      def self.loggable_event(event)
+        self.loggable_events.merge! event
       end
 
       def log_activity_on_create
-        @@_loggable_events.each do |k,v|
+        self.class.loggable_events.each do |k,v|
           next unless k.eql?(:create)
           if v.eql?(true)
             ActiveAudit::Audit.create obj_id: id, obj_type: self.class.to_s, user_id: audit_user_id, activity: k.to_s, extras: audit_extras
@@ -50,7 +48,7 @@ module ActiveAudit
       end
 
       def log_activity_on_update
-        @@_loggable_events.each do |k,v|
+        self.class.loggable_events.each do |k,v|
           next if [:create,:destroy].include?(k)
           if k.eql?(:update) and v.eql?(true)
             ActiveAudit::Audit.create obj_id: id, obj_type: self.class.to_s, user_id: audit_user_id, activity: k.to_s, extras: audit_extras
@@ -74,7 +72,7 @@ module ActiveAudit
       end
 
       def log_activity_on_destroy
-        @@_loggable_events.each do |k,v|
+        self.class.loggable_events.each do |k,v|
           next unless k.eql?(:destroy)
           ActiveAudit::Audit.create obj_id: id, obj_type: self.class.to_s, user_id: audit_user_id, activity: k.to_s, extras: audit_extras
         end
